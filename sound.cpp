6409820ef5d3258490051e709db7e1c466d8b607
Paul Saikko <paul.saikko@cs.helsinki.fi>
2016-12-04 03:05:36 +0200

    add sound channel 4 (noise)

--- sound.cpp
+++ sound.cpp
@@ -127,7 +127,7 @@ struct Sound::CH3 {
 struct Sound::CH4 {
 	union {
 		struct {
-			uint8_t length : 6;
+			uint8_t sound_length : 6;
 			uint8_t _1     : 2;
 		}; 
 		uint8_t NR41;
@@ -308,12 +308,16 @@ void Sound::update(unsigned tclk) {
 		if (Control->sound_on) {
 			uint8_t ch1_sample = updateCh1();
 			uint8_t ch2_sample = updateCh2();
+			uint8_t ch4_sample = updateCh4();
 
 			if (Control->CH1_SO1) new_lsample += ch1_sample;
 			if (Control->CH1_SO2) new_rsample += ch1_sample;
 
 			if (Control->CH2_SO1) new_lsample += ch2_sample;
 			if (Control->CH2_SO2) new_rsample += ch2_sample;
+
+			if (Control->CH4_SO1) new_lsample += ch4_sample;
+			if (Control->CH4_SO2) new_rsample += ch4_sample;
 
 			new_lsample = new_lsample > 255 ? 255 : new_lsample;
 			new_rsample = new_rsample > 255 ? 255 : new_rsample;
@@ -356,6 +360,8 @@ uint8_t Sound::updateCh1() {
 		env_step = float(Channel1->env_sweep) / 64.0f;
 		env_ctr  = 0;
 		vol = Channel1->env_volume << 4;
+
+		Control->CH1_on = true;
 		printf("[ch1] init\n");
 	}
 
@@ -372,8 +378,11 @@ uint8_t Sound::updateCh1() {
 
 		if (Channel1->no_loop) {
 			length -= 1.0f / float(SAMPLE_RATE);
-			if (length <= 0) active = false;
-			printf("[ch1] stop\n");
+			if (length <= 0) {
+				active = false;
+				Control->CH1_on = false;
+				printf("[ch1] stop\n");
+			}
 		}
 
 		if (Channel1->env_sweep != 0) {
@@ -415,6 +424,8 @@ uint8_t Sound::updateCh2() {
 		env_step = float(Channel2->env_sweep) / 64.0f;
 		env_ctr  = 0;
 		vol = Channel2->env_volume << 4;
+
+		Control->CH2_on = true;
 		printf("[ch2] init\n");
 	}
 
@@ -427,14 +438,15 @@ uint8_t Sound::updateCh2() {
 		const static float duty_map[4] { 1.0f/8, 1.0f/4, 1.0f/2, 3.0f/4 };
 		bool low = ((++ctr) % wavelen) > (duty_map[Channel2->wave_duty] * wavelen);
 
-		// TODO: volume
-
 		sample = low ? 0 : vol;
 
-		if (Channel2->no_loop) {
-			printf("[ch2] stop\n");
+		if (Channel2->no_loop) {			
 			length -= 1.0f / float(SAMPLE_RATE);
-			if (length <= 0) active = false;
+			if (length <= 0) {
+				active = false;
+				Control->CH2_on = false;
+				printf("[ch2] stop\n");
+			}
 		}
 
 		if (Channel2->env_sweep != 0) {
@@ -460,5 +472,78 @@ uint8_t Sound::updateCh3() {
 }
 
 uint8_t Sound::updateCh4() {
-	return 0;
+	static bool active = false;
+	static unsigned ctr = 0;
+	static float length = 0;
+	static float env_step = 0;
+	static float env_ctr = 0;
+	static uint8_t vol = 0;
+	static bool low = 0;
+
+	uint8_t sample = 0;
+
+	if (Channel4->init) {
+		active = true;
+		Channel4->init = false;
+		if (!Channel4->no_loop)
+			length = float(64 - Channel4->sound_length) / 256;
+		ctr = 0;
+
+		env_step = float(Channel4->env_sweep) / 64.0f;
+		env_ctr  = 0;
+		vol = Channel4->env_volume << 4;
+
+		Control->CH4_on = true;
+		printf("[ch4] init\n");
+	}
+
+	if (active) {
+		/*
+	union {
+		struct {
+			uint8_t freq_div : 3; // r
+			uint8_t counter_step : 1; // 0=15 bits, 1=7 bits
+			uint8_t shift_clk_freq : 4; // s
+			// freq = 524288 / r / 2^(s+1) Hz  (assume r=0.5 when 0)
+		}; 
+		uint8_t NR43;
+	};
+
+		*/
+		float r = Channel4->freq_div;
+		if (r == 0) r = 0.5f;
+		unsigned s = Channel4->shift_clk_freq;
+		float hz = 524288.0f / r / (2 << (s + 1));
+		unsigned wavelen = unsigned(SAMPLE_RATE / hz);
+		if (wavelen == 0) wavelen = 1;
+		// TODO: Channel5->counter_step
+
+		if (((++ctr) % wavelen) > (wavelen / 2)) low = rand() % 2;
+		sample = low ? 0 : vol;
+
+		if (Channel4->no_loop) {
+			length -= 1.0f / float(SAMPLE_RATE);
+			if (length <= 0) {
+				active = false;
+				Control->CH4_on = false;
+				printf("[ch4] stop\n");
+			}
+		}
+
+		if (Channel4->env_sweep != 0) {
+			env_ctr += 1.0f / float(SAMPLE_RATE);
+			if (env_ctr > env_step) {
+				env_ctr -= env_step;
+				if (Channel4->env_direction == Increase) {
+					printf("[ch4] vol=%02X env+\n", vol);
+					if (vol != 0xF0) vol += 0x10;
+				} else {
+					printf("[ch4] vol=%02X env-\n", vol);
+					if (vol & 0xF0) vol -= 0x10;
+				}
+			}
+		}
+	}
+
+	return sample;
 }
